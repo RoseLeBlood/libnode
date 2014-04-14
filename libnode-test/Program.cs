e9d170dbe17d89ad1e4f9efacb8660fc0c49cb6f
Anna-Sophia Schröck <annasophia.schroeck@outlook.de>
2014-04-15 01:39:20 +0200

    first

---
+++ libnode-test/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using node;
+
+
+namespace libnodetest
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+            Int32BinaryTreeNode node = new Int32BinaryTreeNode ("value1", 2);
+            node.setNode (new Int32BinaryTreeNode ("value2", 34));
+            node.setNode (new Int32BinaryTreeNode ("value3", 35));
+            node.setNode (new Int32BinaryTreeNode ("value4", 166));
+            node.setNode (new Int32BinaryTreeNode ("value5", 78));
+            node.setNode (new Int32BinaryTreeNode ("value6", 89));
+
+
+            Console.WriteLine("Inorder traversal");
+            node.Travers(TraversOrder.Inorder, node.Root);
+            Console.WriteLine("\n");
+ 
+            Console.WriteLine("Preorder traversal");
+            node.Travers(TraversOrder.Preorder, node.Root);
+            Console.WriteLine("\n");
+ 
+            Console.WriteLine("Postorder traversal");
+            node.Travers(TraversOrder.Postorder, node.Root);
+            Console.WriteLine("\n");
+
+			Console.ReadLine ();
+		}
+	}
+}
---
+++ node/Node.cs
@@ -0,0 +1,86 @@
+﻿//
+//  Node.cs
+//
+//  Author:
+//       sophia <annasophia.schroeck@outlook.de>
+//
+//  Copyright (c) 2014 sophia
+//
+//  This program is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU Lesser General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU Lesser General Public License for more details.
+//
+//  You should have received a copy of the GNU Lesser General Public License
+//  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+using System;
+
+namespace node
+{
+    public enum TraversOrder
+    {
+        Preorder,
+        Inorder,
+        Postorder
+    }
+	public abstract class Node<T, D> : Disposable
+		where T : class
+	{
+		protected string m_name;
+
+		protected T      m_data;
+		protected D 	m_root;
+		protected D		m_left;
+		protected D 	m_right;
+
+		public string Name 
+		{ 
+			get { return m_name; } 
+			set { m_name = value; }
+		}
+		public T Data
+		{
+			get { return m_data; }
+			set { m_data = value; }
+		}
+		protected Node (string name) 
+		{
+			m_name = name;
+		}
+		protected Node(string name, T data)
+		{
+			m_name = name;
+			m_data = data;
+		}
+			
+		public virtual D Root { get; protected set; }
+
+		public abstract D getNode(string name);
+		public abstract D setNode(D node);
+		public abstract D removeNode(D node);
+		public abstract D removeNode(string name);
+
+        public abstract void Travers(TraversOrder order, D Root);
+
+       
+		public override string ToString ()
+		{
+			return string.Format ("[Node: Name={0}, Data={1}, Root={2}]", Name, Data, Root);
+		}
+		protected override void Dispose (bool disposing)
+		{
+			base.Dispose (disposing);
+			if (disposing)
+			{
+				m_data = null;
+			}
+		}
+        protected abstract D setParent(D node);
+	}
+}
+
